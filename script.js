5a2da6190167099052b6bbbf046452b677363468
Karan Mann <62376840+karanmann@users.noreply.github.com>
2020-10-16 12:21:14 +0200

    Basic Meme Generator

--- script.js
+++ script.js
@@ -1,15 +1,16 @@
-// Update image onto the screen
 
-const updateImage = () => {
-    let img = document.querySelector('img'); //This returns the first img element.
-    let file = document.querySelector('input[type=file').files[0]; // Returns the first file element found.
+
+// Update image onto the screen
+function update_image() {
+    var img = document.querySelector('img'); // Returns the first img element
+    var file = document.querySelector('input[type=file]').files[0]; // Returns the first file element found
     img.src = window.URL.createObjectURL(file);
+
 }
 
-// Write the test onto the meme
-
-const updateText = () => {
-    let userText = document.getElementById('userText');
-    let memeText = document.getElementById('memeText');
-    memeText.innerHTML = userText.nodeValue;
+// Write the text onto the meme
+function update_text() {
+    var user_text = document.getElementById("user_text");
+    var meme_text = document.getElementById("meme_text");
+    meme_text.innerHTML = user_text.value;
 }
